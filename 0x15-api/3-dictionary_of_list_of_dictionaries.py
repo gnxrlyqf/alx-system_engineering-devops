f6537b5f243328c9128c57855901da6c284161fd
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-30 02:56:38 +0100

    3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""gather data from API"""
+from requests import get
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    users = get("https://jsonplaceholder.typicode.com/users/").json()
+    tasks = get("https://jsonplaceholder.typicode.com/todos").json()
+    all = {}
+    for user in users:
+        tasks_list = []
+        for task in tasks:
+            if task["userId"] == user["id"]:
+                dict = {
+                    "username": user["username"],
+                    "task": task["title"],
+                    "completed": task["completed"],
+                }
+                tasks_list.append(dict)
+    all[user.get('id')] = tasks
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(all, file)
