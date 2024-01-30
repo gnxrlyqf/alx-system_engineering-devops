a5a2e26ff4fd5bbd1c2f7fc931945937881fc936
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-30 02:28:15 +0100

    1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/"
     tasks = get("https://jsonplaceholder.typicode.com/todos").json()
     user = get("{}/{}".format(url, argv[1]))
-    name = user.json().get('name')
+    name = user.json().get("name")
     total = []
     completed = []
     for task in tasks:
-        if task.get('userId') == int(argv[1]):
+        if task.get("userId") == int(argv[1]):
             total.append(task["title"])
-            if task.get('completed'):
+            if task.get("completed"):
                 completed.append(task["title"])
     string = "Employee {} is done with tasks({}/{}):"
     print(string.format(name, len(completed), len(total)))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""gather data from API"""
+from requests import get
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    tasks = get("https://jsonplaceholder.typicode.com/todos").json()
+    user = get("{}/{}".format(url, argv[1]))
+    name = user.json().get("name")
+    with open(argv[1] + '.csv', 'w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for task in tasks:
+        if task.get('userId') == int(argv[1]):
+            row = [argv[1], name, str(task["completed"]), task["title"]]
+            writer.writerow(row)
