1355a7d84853674a7c916e4a87fe73716cc2ba88
gnxrlyqf <gnarlydestructor@outlook.com>
2024-02-13 23:45:37 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     user = {"User-Agent": "My-User-Agent"}
-    url = "https://www.reddit.com/dev/api/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     with get(url, headers=user, allow_redirects=False) as page:
         if page.status_code >= 300:
-            return 0
+            return page
         data = page.json()
         return data["data"]["subscribers"]
